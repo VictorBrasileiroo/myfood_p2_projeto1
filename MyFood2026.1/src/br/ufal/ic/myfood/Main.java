0e269becc67a7d5573184d60344feb4da8e1d61b
Victor André Lopes Brasileiro <victor.lopesvfk11@gmail.com>
2026-04-18 21:36:32 -0300

    fix: ajuste da main para compilar corretamente

--- MyFood2026.1/src/br/ufal/ic/myfood/Main.java
+++ MyFood2026.1/src/br/ufal/ic/myfood/Main.java
@@ -1,16 +1,37 @@
 package br.ufal.ic.myfood;
 
 import easyaccept.EasyAccept;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
-        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", "MyFood2026.1/tests/us1_1.txt"});
-        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", "MyFood2026.1/tests/us1_2.txt"});
-        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", "MyFood2026.1/tests/us2_1.txt"});
-        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", "MyFood2026.1/tests/us2_2.txt"});
-        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", "MyFood2026.1/tests/us3_1.txt"});
-        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", "MyFood2026.1/tests/us3_2.txt"});
-        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", "MyFood2026.1/tests/us4_1.txt"});
-        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", "MyFood2026.1/tests/us4_2.txt"});
+        String base = findTestBase();
+        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", base + "us1_1.txt"});
+        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", base + "us1_2.txt"});
+        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", base + "us2_1.txt"});
+        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", base + "us2_2.txt"});
+        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", base + "us3_1.txt"});
+        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", base + "us3_2.txt"});
+        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", base + "us4_1.txt"});
+        EasyAccept.main(new String[] {"br.ufal.ic.myfood.Facade", base + "us4_2.txt"});
+    }
+
+    private static String findTestBase() {
+        // (MyFood2026.1/)
+        if (new File("tests/us1_1.txt").exists()) return "tests/";
+        // raiz do repo 
+        if (new File("MyFood2026.1/tests/us1_1.txt").exists()) return "MyFood2026.1/tests/";
+        // pasta extraída do zip
+        File[] subdirs = new File(".").listFiles(File::isDirectory);
+        if (subdirs != null) {
+            for (File sub : subdirs) {
+                String path = sub.getName() + "/MyFood2026.1/tests/";
+                if (new File(path + "us1_1.txt").exists()) return path;
+            }
+        }
+        throw new IllegalStateException(
+                "Nao foi possivel localizar a pasta de testes. Diretorio atual: "
+                        + new File(".").getAbsolutePath()
+        );
     }
 }
